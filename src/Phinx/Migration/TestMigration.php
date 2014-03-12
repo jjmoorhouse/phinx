568e8f3a8d2cc06bc0c398768bdc35d6603149c9
Andu Aanei <andu@rhonda-ThinkPad-T520.(none)>
2014-03-12 10:20:40 +0000

    Added TestMigration class.

--- src/Phinx/Migration/TestMigration.php
+++ src/Phinx/Migration/TestMigration.php
@@ -1,16 +1,20 @@
 <?php
 /**
- * 
+ * Abstract Test Migration Class
  *
+ * Migrations used for test data should extend this class.
  *
+ * It provides a single method which used to be in a separate trait.
  *
  */
 
 namespace Phinx\Migration;
 
 
-class TestMigration extends AbstractMigration {
-    protected function insertTestData($table, array $data) {
+abstract class TestMigration extends AbstractMigration
+{
+    protected function insertTestData($table, array $data)
+    {
         $rows = array();
         foreach ($data as $row) {
             $rows[] = implode('","',$row);
